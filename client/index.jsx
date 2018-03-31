5ce7b6e724d31f55301557582b9c77b3597759ca
mabrsweeney <mabrsweeney@gmail.com>
2018-03-31 12:06:54 -0700

    uses axios call to get initial data

---
+++ client/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import TextLine from './components/TextLine';
+import styles from './public/styles.css';
+import axios from 'axios';
+axios.get(`/challenge/${window.location.href.split('/')[4]}/sentence`).then( response => {
+  ReactDom.render(<TextLine text={response.data}/>, document.getElementById('text-line'));
+})
