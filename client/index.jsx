0a40a48b2539b058ad441885e94aaf1582077e1e
mabrsweeney <mabrsweeney@gmail.com>
2018-03-31 15:49:34 -0700

    changed prop name to reflect highscore data

--- client/index.jsx
+++ client/index.jsx
@@ -4,5 +4,5 @@ import TextLine from './components/TextLine';
 import styles from './public/styles.css';
 import axios from 'axios';
 axios.get(`/challenge/${window.location.href.split('/')[4]}/sentence`).then( response => {
-  ReactDom.render(<TextLine text={response.data}/>, document.getElementById('text-line'));
+  ReactDom.render(<TextLine textHighscores={response.data}/>, document.getElementById('text-line'));
 })
