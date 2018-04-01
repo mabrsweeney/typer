73044dc191a6ff41df6a3d11f8d7bdabacacc451
mabrsweeney <mabrsweeney@gmail.com>
2018-03-31 20:31:54 -0700

    Removed list button

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Header = () => {
   return (<div className="header">
   <h1 className="title">Typer Challenge</h1>
-  <h2 className="link-button">Challenge List</h2>
   <h2 className="link-button">Create A Challenge</h2>
   <h2 className="link-button">Random Challenge</h2>
   </div>)
