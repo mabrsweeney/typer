9a5d1886cf357c17aea555d61bdf57186156fabf
mabrsweeney <mabrsweeney@gmail.com>
2018-04-01 13:03:39 -0700

    header background color

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 
-const Header = () => {
-  return (<div className="header">
+const setBG = (difficulty) => {
+  if (difficulty >= 30) {
+    return 'hard-header';
+  } else if (difficulty < 30 && difficulty >= 20){
+    return 'medium-header';
+  } 
+  return 'easy-header';
+}
+
+const submitChallenge = () => {
+  const newChallenge = prompt(
+    `Enter a new challenge:
+  (Max length: 500 characters)`);
+  axios.post('/submit/challenge', {challenge: newChallenge})
+  .then(response => console.log('Success'))
+  .catch(err => console.log('Error'));
+}
+
+const Header = (props) => {
+  return (<div className={`header ${setBG(props.difficulty)}`}>
   <h1 className="title">Typer Challenge</h1>
-  <a className="link-button">Create A Challenge</a>
+  <button className="link-button" onClick={submitChallenge}>Create A Challenge</button>
   <a className="link-button" href={`http://localhost:3000/challenge/${Math.floor(Math.random() * 1000)}`}>Random Challenge</a>
   </div>)
 }
