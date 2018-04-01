6d28061c0a8d3eb4e3b8a7dd2b2b29298267b816
mabrsweeney <mabrsweeney@gmail.com>
2018-04-01 05:50:30 -0700

    Moved imports to sql file

--- db/pop.js
+++
@@ -1,4 +0,0 @@
-use typer;
-LOAD DATA LOCAL INFILE '/Users/matthewsweeney/typer/db/players.csv' INTO TABLE players LINES TERMINATED BY '\n' (player_name);
-LOAD DATA LOCAL INFILE '/Users/matthewsweeney/typer/db/sentences.csv' INTO TABLE sentences COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n' (sentence, num_attempts, difficulty);
-LOAD DATA LOCAL INFILE '/Users/matthewsweeney/typer/db/scores.csv' INTO TABLE scores COLUMNS TERMINATED BY ',' LINES TERMINATED BY '\n' (score, pid, sent_id);
