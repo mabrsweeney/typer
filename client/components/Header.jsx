663202f769ba2319f18b9471f6c0a0316e18e36f
mabrsweeney <mabrsweeney@gmail.com>
2018-03-31 22:24:37 -0700

    added link for a random challenge

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
   return (<div className="header">
   <h1 className="title">Typer Challenge</h1>
-  <h2 className="link-button">Create A Challenge</h2>
-  <h2 className="link-button">Random Challenge</h2>
+  <a className="link-button">Create A Challenge</a>
+  <a className="link-button" href={`http://localhost:3000/challenge/${Math.floor(Math.random() * 1000)}`}>Random Challenge</a>
   </div>)
 }
 
