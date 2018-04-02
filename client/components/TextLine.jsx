94915a8307cd99857ac41effb17bdba82fa55e02
mabrsweeney <mabrsweeney@gmail.com>
2018-04-02 10:33:36 -0700

    removed listener when challenge completes

--- client/components/TextLine.jsx
+++ client/components/TextLine.jsx
@@ -1,85 +1,83 @@
 import React from 'react';
+import $ from 'jquery';
 import Timer from './Timer';
 import Scores from './Scores';
 import Header from './Header';
-import $ from 'jquery';
 
 export default class TextLine extends React.Component {
   constructor(props) {
-     console.log(props);
+    console.log(props);
     super(props);
     this.state = {
       correct: '',
       incorrect: 0,
       next: this.props.textHighscores.sentence,
-      complete: '',
       ready: false,
       typeCount: 0,
       done: false,
       time: 0,
-    }
+    };
     this.checkLetter = this.checkLetter.bind(this);
+    this.checkListener = this.checkListener.bind(this);
   }
 
   componentDidMount() {
-
-    //Set background color based on difficulty of challenge
-    const difficulty = this.props.textHighscores.difficulty;
+    // Set background color based on difficulty of challenge
+    const { difficulty } = this.props.textHighscores;
     if (difficulty >= 30) {
       $('body').addClass('hardbg');
-    } else if (difficulty < 30 && difficulty >= 20){
+    } else if (difficulty < 30 && difficulty >= 20) {
       $('body').addClass('mediumbg');
     } else {
       $('body').addClass('easybg');
     }
-
-    const TL = this;
-    document.body.addEventListener('keyup', function(event) {
-      TL.checkLetter(event.key);
-    });
+    window.addEventListener('keyup', this.checkListener);
   }
 
+  checkListener(event) {
+    this.checkLetter(event.key);
+  }
 
   checkLetter(key) {
-    if (key === this.state.next[0] ) {
+    console.log(this.state.done);
+    if (key === this.state.next[0]) {
       const addition = this.state.next[0];
       const count = this.state.typeCount + 1;
       const newNext = this.state.next.substr(1);
       this.setState({
         correct: this.state.correct + addition,
-        next: newNext
-      })
-
+        next: newNext,
+      });
       if (newNext.length === 0) {
         this.setState({
-          complete: 'Complete!',
-          done: true
+          done: true,
         });
+        window.removeEventListener('keyup', this.checkListener);
       }
-    } else if(key.length === 1){
+    } else if (key.length === 1) {
       this.setState({
         incorrect: this.state.incorrect + 1,
       });
     }
     if (this.state.typeCount === 0) {
       this.setState({
-        ready: true
+        ready: true,
       });
     }
-    this.setState({typeCount: this.state.typeCount+1});
+    this.setState({ typeCount: this.state.typeCount + 1 });
   }
 
   render() {
     return (
       <div>
-        <Header difficulty={this.props.textHighscores.difficulty}/>
+        <Header difficulty={this.props.textHighscores.difficulty} />
         <div className="text-line-container" >
           <div className="text-area">
             <pre className="tl correct">{this.state.correct}</pre>
             <pre className="tl next">{this.state.next}</pre>
           </div>
           <p> Mistakes: {this.state.incorrect}</p>
-          <Timer sid={this.props.textHighscores.sent_id} start={Date.now()} ready={this.state.ready} done={this.state.done} saveTime={this.saveTime}/>
+          <Timer sid={this.props.textHighscores.sent_id} start={Date.now()} ready={this.state.ready} done={this.state.done} saveTime={this.saveTime} />
           <Scores className="scores" scores={this.props.textHighscores.scores} />
         </div>
       </div>
