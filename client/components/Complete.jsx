ce6811465028a0c334abd8dfe06bff92ffa16b5c
mabrsweeney <mabrsweeney@gmail.com>
2018-04-02 11:00:03 -0700

    prevents multiple submissions

--- client/components/Complete.jsx
+++ client/components/Complete.jsx
@@ -2,34 +2,39 @@ import React from 'react';
 
 export default class Complete extends React.Component {
   constructor(props) {
-    super(props)
-    this.state ={name: ''};
+    super(props);
+    this.state = {
+      name: '',
+      submitted: false,
+    };
     this.updateName = this.updateName.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   updateName(e) {
     e.preventDefault();
-    this.setState({ name: e.target.value});
+    this.setState({ name: e.target.value });
   }
-  
+
   handleSubmit(e) {
     e.preventDefault();
-    this.props.postScore(this.state.name);
+    if (!this.state.submitted) {
+      this.props.postScore(this.state.name);
+      this.setState({ submitted: true });
+    }
   }
 
   render() {
-    if(this.props.done) {
+    if (this.props.done) {
       return (
         <div>
           <h1>Complete!</h1>
           <form onSubmit={e => this.handleSubmit(e)}>
-            <input type="text" name="name" placeholder="Enter Player Name..." onChange={e => this.updateName(e)}/>
+            <input type="text" name="name" placeholder="Enter Player Name..." onChange={e => this.updateName(e)} />
             <input type="submit" />
           </form>
         </div>);
-    } else {
-      return null;
     }
+    return null;
   }
 }
