429a2e59abd5ee3b204bc2cc0fba48ad6bcb3d38
mabrsweeney <mabrsweeney@gmail.com>
2018-04-02 14:11:02 -0700

    no longer retrieves data here

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import axios from 'axios';
 import TextLine from './components/TextLine';
 import styles from './public/styles.css';
 
-axios.get(`/challenge/${window.location.href.split('/')[4]}/sentence`).then((response) => {
-  ReactDom.render(<TextLine textHighscores={response.data} />, document.getElementById('text-line'));
-});
+ReactDom.render(<TextLine />, document.getElementById('text-line'));
+
