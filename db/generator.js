b13af2241f633efdab760a730188e1bdb2fad3ef
mabrsweeney <mabrsweeney@gmail.com>
2018-04-02 13:32:55 -0700

    generates correct data schema

--- db/generator.js
+++ db/generator.js
@@ -1,23 +1,18 @@
 const faker = require('faker');
 const fs = require('fs');
 
-const getSentence = () => {
-  return faker.lorem.sentence();
-};
-const getPlayer = () => {
-  return faker.internet.userName();
-};
+const getSentence = () => faker.lorem.sentence();
+const getPlayer = () => faker.internet.userName();
 const getScore = () => {
-  let num = String(faker.random.number({min:1, max:60}));
-  num += `.${faker.random.number({min:1, max:99})}`
+  let num = String(faker.random.number({ min: 1, max: 60 }));
+  num += `.${faker.random.number({ min: 1, max: 99 })}`;
   return num;
-
 };
 
 // (id INT PRIMARY KEY AUTO_INCREMENT, sentence VARCHAR(400),num_attempts INT,difficulty INT)
 let sentences = '';
 for (let i = 1; i <= 1000; i++) {
-  sentences += `${getSentence()},0,0\n`
+  sentences += `${getSentence()},0,0\n`;
 }
 fs.writeFile('./db/sentences.csv', sentences, (err) => {
   if (err) throw err;
@@ -27,7 +22,7 @@ fs.writeFile('./db/sentences.csv', sentences, (err) => {
 // (id INT PRIMARY KEY AUTO_INCREMENT, player_name VARCHAR(50) UNIQUE)
 let players = '';
 for (let i = 1; i <= 100; i++) {
-  players += `${getPlayer()}\n`
+  players += `${getPlayer()}\n`;
 }
 fs.writeFile('./db/players.csv', players, (err) => {
   if (err) throw err;
@@ -37,7 +32,7 @@ fs.writeFile('./db/players.csv', players, (err) => {
 // (score VARCHAR(6), pid INT, sent_id INT)
 let scores = '';
 for (let i = 1; i <= 10000; i++) {
-  scores += `${getScore()},${faker.random.number({min:1, max:100})},${faker.random.number({min:1, max:1000})}\n`
+  scores += `${getScore()},${faker.random.number({ min: 1, max: 100 })},${faker.random.number({ min: 1, max: 1000 })}\n`;
 }
 fs.writeFile('./db/scores.csv', scores, (err) => {
   if (err) throw err;
